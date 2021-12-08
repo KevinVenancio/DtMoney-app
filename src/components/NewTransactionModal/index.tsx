b4b155bb31b2007c47e650d16aaac8909ac76311
Kevin Venâncio <83186522+Kevin414gamer@users.noreply.github.com>
2021-12-07 22:23:03 -0300

    Update index.tsx

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -17,7 +17,7 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
   const { createTransaction } = useTransactions();
 
   const [title, setTitle] = useState('');
-  const [amount, setAmount] = useState();
+  const [amount, setAmount] = useState(0);
   const [category, setCategory] = useState('');
   const [type, setType] = useState('deposit');
 
